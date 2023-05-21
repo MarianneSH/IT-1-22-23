85e5ad49aa7cc1d7afd5dac304f76ec81a5403af
MarianneSH <113419513+MarianneSH@users.noreply.github.com>
2023-05-21 15:53:26 +0200

    Add files via upload

---
+++ VisittkortOppgave/visitkortData.js
@@ -0,0 +1,30 @@
+/*
+var visittInfo = {
+    "person":[{"navn":"Per", "kjønn": "Mann", "bursdag": "17.08.2002", "Yrke": "Kiwi", "Telefon": "63729372", "Forerkort": "Nei","Bilde": "per.jpg"},
+              {"navn":"Kristian", "kjønn": "Annet", "bursdag": "13.12.1997", "Yrke": "XXL", "Telefon": "99999989", "Forerkort": "Ja","Bilde": "kristian.jpeg"},
+              {"navn":"Amalie", "kjønn": "Kvinne", "bursdag": "31.01.1990", "Yrke": "Lege", "Telefon": "12345678", "Forerkort": "Ja","Bilde": "amalie.jpeg"},
+              {"navn":"Liam", "kjønn": "Mann", "bursdag": "04.03.1985", "Yrke": "Sivilingeniør", "Telefon": "98765432", "Forerkort": "Ja","Bilde": "liam.jpeg"}
+]
+}
+*/
+/*
+var visittInfo = {
+    "person": [
+        {"navn":"Navn", "kjønn": "Kjønn", "bursdag": "Fødselsdag", "yrke": "Yrke", "telefon": "Telefonnummer", "forerkort": "Førerkort","bildet": "hundPortrett.avif"},
+        {"navn":"Per", "kjønn": "Mann", "bursdag": "17.08.2002", "yrke": "Kiwi", "telefon": "63729372", "forerkort": "Nei","bildet": "per.jpg" },
+        {"navn":"Kristian", "kjønn": "Annet", "bursdag": "13.12.1997", "yrke": "XXL", "telefon": "99999989", "forerkort": "Ja","bildet": "kristian.jpeg"},
+        {"navn":"Amalie", "kjønn": "Kvinne", "bursdag": "31.01.1990", "yrke": "Lege", "telefon": "12345678", "forerkort": "Ja","bildet": "amalie.jpeg"},
+        {"navn":"Liam", "kjønn": "Mann", "bursdag": "04.03.1985", "yrke": "Sivilingeniør", "telefon": "98765432", "forerkort": "Ja","bildet": "liam.jpeg"}
+    ]
+};
+*/
+
+var visittInfo = {
+    "person": [
+        {"navn":"Navn", "kjoonn": "Kjønn", "bursdag": "Fødselsdag", "yrke": "Yrke", "telefon": "Telefonnummer", "forerkort": "Førerkort","bildet": "hundPortrettNy.jpg", "bakgrunnen": "#FFFFFF", "teksten": "#000000", "grensene": "#000000", "grenseStil": ""},
+        {"navn":"Per", "kjoonn": "Mann", "bursdag": "2002-08-17", "yrke": "Kiwi", "telefon": "63729372", "forerkort": "Har ikke førerkort","bildet": "per.jpg", "bakgrunnen": "#FFC300", "teksten": "#336699", "grensene": "#E5243B", "grenseStil": "double" },
+        {"navn":"Kristian", "kjoonn": "Annet", "bursdag": "1997-12-13", "yrke": "XXL", "telefon": "99999989", "forerkort": "Har førerkort","bildet": "kristian.jpeg", "bakgrunnen": "#F8E7A2", "teksten": "#A7D7C5", "grensene": "#E3B5A5", "grenseStil": "dotted"},
+        {"navn":"Amalie", "kjoonn": "Dame", "bursdag": "1990-01-31", "yrke": "Lege", "telefon": "12345678", "forerkort": "Har førerkort","bildet": "amalie.jpeg", "bakgrunnen": "#92C4AF", "teksten": "#F5E0A4", "grensene": "#C39BD3", "grenseStil": "double"},
+        {"navn":"Liam", "kjoonn": "Mann", "bursdag": "1985-04-03", "yrke": "Sivilingeniør", "telefon": "98765432", "forerkort": "Har førerkort","bildet": "nyLiam.jpeg", "bakgrunnen": "#FFD699", "teksten": "#D1C4E9", "grensene": "#B2EBF2", "grenseStil": "solid"}
+    ]
+};
